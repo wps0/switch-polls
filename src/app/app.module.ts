837b4964458e00013f976d6f96a1b90a0ffc9f8f
PW <53907986+wps0@users.noreply.github.com>
2021-09-15 20:39:05 +0200

    design tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PollViewComponent } from './views/poll-view/poll-view.component';
-import { AppRoutingModule } from '@app/routing/app-routing.module';
-import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { PollViewComponent } from '@views/poll-view/poll-view.component';
+import { AppRoutingModule } from '@app/modules/app-routing.module';
+import { PageNotFoundComponent } from '@views/page-not-found/page-not-found.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '@app/material.module';
+import { MaterialModule } from '@app/modules/material.module';
+import { LogoComponent } from './shared/components/logo/logo.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, PollViewComponent, PageNotFoundComponent],
+  declarations: [
+    AppComponent,
+    PollViewComponent,
+    PageNotFoundComponent,
+    LogoComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/views/poll-view/poll-view.component.ts
+++ src/app/views/poll-view/poll-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PollInterface } from '@shared/Poll.interface';
+import { PollInterface } from '@shared/models/Poll.interface';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
