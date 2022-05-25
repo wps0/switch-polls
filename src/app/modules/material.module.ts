fb52cfa6f9af7ddae3869f0b913f6ad1ce6952f9
PW <53907986+wps0@users.noreply.github.com>
2022-05-25 15:22:55 +0200

    cleanup
    Signed-off-by: PW <53907986+wps0@users.noreply.github.com>

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -16,8 +14,6 @@ import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   exports: [
     MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
     MatIconModule,
     MatInputModule,
     MatRadioModule,
